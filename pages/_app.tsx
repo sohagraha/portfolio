278d9ee538a1ea59b89f5b80a636ec892fa547fb
sohagraha <sohagrahabd@gmail.com>
2022-07-26 13:34:56 +0600

    style : remove and add padding on top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return <ThemeProvider attribute="class">
-    <div className="grid grid-cols-12 gap-6 px-5 py-14 lg:mb-0 md:mb-16 sm:px-15 md:px-20 lg:px-24 xl:px-30 2xl:px-36">
+    <div className="grid grid-cols-12 gap-6 px-5 py-4 sm:py-14 lg:mb-0 md:mb-16 sm:px-15 md:px-20 lg:px-24 xl:px-30 2xl:px-36">
       <div className="col-span-12 p-4 text-base text-center bg-white h-fit dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark dark:border-t-2 dark:border-gray-600">
         <Sidebar />
       </div>
